3630623e5fa3689c5f5af4f93b7bb88e42e8047e
0x0 <ghstoreinvesting@gmail.com>
2018-08-13 02:41:10 +0800

    Add some tests

--- routes/api.php
+++ routes/api.php
@@ -17,7 +17,12 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::group(['middleware' => 'auth:api'], function () {
+    Route::post('logout', 'Auth\LoginController@logout');
+});
+
 Route::group(['middleware' => 'guest:api'], function () {
     Route::post('register', 'Auth\RegisterController@register');
     Route::post('login', 'Auth\LoginController@login');
+
 });
